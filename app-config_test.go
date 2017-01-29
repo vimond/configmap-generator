77d9c8c2190c719c610c108d75714fcdc689b237
pbthorste <pbthorste@users.noreply.github.com>
2017-01-29 21:30:18 +0100

    change path to test files

--- app-config_test.go
+++ app-config_test.go
@@ -2,12 +2,10 @@ package configmap_generator
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestLoadConfig(t *testing.T) {
 	appConfig := LoadConfig()
-	fmt.Println(appConfig)
 	expectedName := "vimond-docker-thumbor"
 	actualName := appConfig.Applications[0].Name
 	if actualName != expectedName {
